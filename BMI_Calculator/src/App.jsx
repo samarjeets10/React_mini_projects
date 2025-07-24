ec5d927cee6babe8fda01ac30228f1f37651063b
Samarjeet <samarsabale1021@gmail.com>
2025-07-24 08:42:58 +0530

    Add initial layout and styling for BMI Calculator

--- BMI_Calculator/src/App.jsx
+++ BMI_Calculator/src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import {  } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <div className="wrapper">
+        <div className="container">
+          <div className="container-header">
+            <h2>BMI Calculator</h2>
+          </div>
+
+          <form action="" className="inputForm">
+            <div className="weight-content">
+              <label htmlFor="weight">Weight <span>(Kg)</span></label>
+              <input type="text" id='weight' placeholder='enter weight here..'/>
+            </div>
+            <div className="height-content">
+              <label htmlFor="height">Height <span>(cm)</span></label>
+              <input type="text" id='height' placeholder='enter height here..'/>
+            </div>
+          </form>
+
+          <div className="calculate">
+            <button>Calculate Your BMI</button>
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }
