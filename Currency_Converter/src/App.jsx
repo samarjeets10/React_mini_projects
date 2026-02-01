9c86cf8936c651dbce0d0114b64c35f1bf8090e6
Samarjeet <samarsabale1021@gmail.com>
2026-02-01 09:36:04 +0530

    feat: initialize Currency Converter project with React, Vite, and Tailwind CSS
    - Implement custom hook for fetching currency information
    - Set up Vite configuration for React and Tailwind CSS

---
+++ Currency_Converter/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+
+  
+
+  return (
+    <>
+
+      <div className='h-screen bg-gray-800'>
+        <h1 className='text-white text-2xl font-semibold'>Currency Converter</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
