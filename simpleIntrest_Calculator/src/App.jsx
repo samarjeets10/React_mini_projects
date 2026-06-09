1a5645a356ef863c31a32dc6d68bb9c80dffab95
Samarjeet <samarsabale1021@gmail.com>
2026-06-09 23:13:19 +0530

    feat: enhance Card component layout and styling for improved user experience.

--- simpleIntrest_Calculator/src/App.jsx
+++ simpleIntrest_Calculator/src/App.jsx
@@ -4,7 +4,7 @@ import Card from './components/Card'
 function App() {
   return (
     <>
-      <div className='w-full min-h-screen px-6 py-6 flex items-center justify-center'>
+      <div className='min-h-screen bg-lime-50 flex items-center justify-center px-12 py-8'>
         <Card />
       </div>
     </>
--- simpleIntrest_Calculator/src/components/Card.jsx
+++ simpleIntrest_Calculator/src/components/Card.jsx
@@ -2,79 +2,110 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='w-full bg-white border-2 border-neutral-200 rounded-2xl'>
-        <div className='w-full h-full px-6 py-8 flex justify-between gap-2'>
-            <div className='w-1/2 h-full py-2'>
-                <div className='w-full'>
-                    <h1 className='text-xl md:text-2xl font-semibold'>Calculate your compound intrest</h1>
-                    <p className='text-sm mt-1 md:text-md text-neutral-500'>Compound intrest is where the intrest you earn, earn intrest.</p>
-                </div>
-
-                <div className='w-full mt-12'>
-
-                    <div className='w-full flex items-center gap-[2rem]'>
-
-                        <div className='w-1/2'>
-                            <h3 className='text-lg font-normal'>Your savings amount</h3>
-                            <div className='mt-3 flex items-center gap-3 text-xl'>
-                                <i class="ri-money-rupee-circle-line"></i>
-                                <input 
-                                className='text-[1.2rem] text-green-600 outline-none border-b-2 border-neutral-300'
-                                type="text"
-                                placeholder='eg. 20000' />
-                            </div>
-                        </div>
-
-                        <div className='w-1/2'>
-                            <h3 className='text-lg font-normal'>Years will be saved</h3>
-                            <div className='mt-3 flex items-center gap-3 text-xl'>
-                                <input 
-                                className='text-[1.2rem] text-green-600 text-md outline-none border-b-2 border-neutral-300'
-                                type="text"
-                                placeholder='100' />
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className='w-full mt-12'>
-
-                    <div className='w-full flex items-center gap-[2rem]'>
-
-                        <div className='w-1/2 flex flex-col gap-6'>
-                            <div className='w-full flex items-center justify-between gap-2'>
-                                <h3 className='text-lg'>Rate of return</h3>
-                                <span className='text-lg text-green-600'>50%</span>
-                            </div>
-                            <input
-                            className='w-full'
-                            type="range" />
-                        </div>
-
-                        <div className='w-1/2 flex flex-col gap-4'>
-                            <h3 className='text-lg font-normal'>Years will be saved</h3>
-                            <div className=' flex items-center gap-3 text-xl'>
-                                <input 
-                                className='text-[1.2rem] text-green-600 text-md outline-none border-b-2 border-neutral-300'
-                                type="text"
-                                placeholder='100' />
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
+    <div className='w-full h-full py-8 px-6 bg-white rounded-2xl border-2 border-neutral-200 flex items-stretch gap-8'>
+        
+        <div className="left w-1/2 h-full py-4 px-2 flex flex-col gap-6">
+            <div>
+                <h2 className='text-2xl font-semibold'>Calculate your compound intrest</h2>
+                <p className='text-md mt-2 text-neutral-500 '>Compound intesrt is when the intrest you earn, earn intrest</p>
             </div>
 
-            <div className='w-1/2 h-full p-4 bg-green-800 rounded-2xl text-white'>
+            <div className='w-full flex mt-2 items-center justify-between gap-12'>
+                <div className='w-full flex flex-col'>
+                    <label className='text-lg font-normal'>Your savings amount</label>
+                    <input 
+                    className='mt-2 py-1 text-md border-b-2 outline-none border-gray-300 text-lg'
+                    type="text" 
+                    placeholder='eg. 20000' />
+                </div>
                 <div className='w-full'>
-                    <h3 className='text-lg md:text-xl font-normal'>Your estimated savings</h3>
-                    <p className='text-sm mt-1 md:text-md text-neutral-300
-                    '>Get estimations</p>
+                    <label className='text-lg font-normal'>Your will be saving</label>
+                    <input 
+                    className='mt-2 py-1 text-md border-b-2 outline-none border-gray-300 text-lg'
+                    type="text" 
+                    placeholder='eg. 100' />
                 </div>
             </div>
+
+            <div className='w-full flex mt-2 items-center justify-between gap-12'>
+                <div className='w-full'>
+                    <label className='text-lg font-normal'>Rate of return</label>
+                    <input 
+                    className='w-full mt-2 py-1 text-md border-b-2 outline-none border-gray-300 text-lg'
+                    type="text" 
+                    placeholder='eg. 10' />
+                </div>
+                <div className='w-full'>
+                    <label className='text-lg font-normal'>How long you will contribute</label>
+                    <input 
+                    className='mt-2 py-1 text-md border-b-2 outline-none border-gray-300 text-lg'
+                    type="text" 
+                    placeholder='per months' />
+                </div>
+            </div>
+
+            <div className='w-full mt-6 flex flex-col gap-4'>
+                <div>
+                    <h3 className='text-xl font-normal'>Prefered deposit method</h3>
+                </div>
+
+                <div className='w-full flex flex-wrap gap-4'>
+                    <div className='w-fit px-2 py-2  rounded-lg flex items-center gap-2 border-1 border-neutral-300 cursor-pointer'>
+                        <input type="radio" />
+                        <p>Savings account</p>
+                    </div>
+                    <div className='w-fit px-2 py-2  rounded-lg flex items-center gap-2 border-1 border-neutral-300 cursor-pointer'>
+                        <input type="radio" />
+                        <p>Money market/High yielding account</p>
+                    </div>
+                    <div className='w-fit px-2 py-2  rounded-lg flex items-center gap-2 border-1 border-neutral-300 cursor-pointer'>
+                        <input type="radio" />
+                        <p>Savings account</p>
+                    </div>
+                    <div className='w-fit px-2 py-2  rounded-lg flex items-center gap-2 border-1 border-neutral-300 cursor-pointer'>
+                        <input type="radio" />
+                        <p>Current account</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
+        <div className='right w-1/2 h-full p-6 bg-green-900 text-white rounded-2xl flex flex-col gap-6'>
+            <div>
+                <h2 className='text-xl font-semibold'>Your estimated saving</h2>
+            </div>
+
+            <div className='w-full pb-4 mt-4 border-b-2 border-neutral-400 flex items-center justify-between gap-2'>
+                <h3 className='text-md text-normal'>Your savings(excludind all taxes)</h3>
+                <span className='text-xl font-semibold flex items-center gap-1'><i class="ri-money-rupee-circle-line"></i> <p>143,982.0</p></span>
+            </div>
+            <div className='w-full pb-4 mt-4 border-b-2 border-neutral-400 flex items-center justify-between gap-2'>
+                <h3 className='text-md text-normal'>Total amount contributed</h3>
+                <span className='text-xl font-semibold flex items-center gap-1'><i class="ri-money-rupee-circle-line"></i> <p>39,890</p></span>
+            </div>
+            <div className='w-full pb-4 mt-4 border-b-2 border-neutral-400 flex items-center justify-between gap-2'>
+                <h3 className='text-md text-normal'>Total intrest</h3>
+                <span className='text-xl font-semibold flex items-center gap-1'><i class="ri-money-rupee-circle-line"></i> <p>9890</p></span>
+            </div>
+
+            <div className='w-full p-4 mt-2 bg-lime-300 rounded-xl text-green-800 flex items-center justify-between'>
+                <h3 className='text-md text-normal'>Your estimated savings</h3>
+                <span className='text-xl font-semibold flex items-center gap-1'><i class="ri-money-rupee-circle-line"></i> <p>9890</p></span>
+            </div>
+
+            <div className='w-full mt-6 flex items-center justify-between gap-4'>
+                <div>
+                    <p className='text-sm'>Compare your savings account yealds by KPIs</p>
+                </div>
+
+                <button className='text-sm px-6 py-2 bg-emerald-600 rounded-md cursor-pointer'>
+                    Compare
+                </button>
+            </div>
+
+        </div>
+
     </div>
   )
 }
