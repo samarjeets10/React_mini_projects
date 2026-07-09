5de586913c640c68ed592cca53ff2584ea2e6672
Samarjeet <samarsabale1021@gmail.com>
2026-07-10 02:08:23 +0530

    feat(cart): enhance order summary and total display with new components.
    - Improve the cart UI by adding a more detailed order summary section.
    
    - Introduce a dedicated total breakdown with subtotal, shipping, tax, and final total.
    
    - Add supporting checkout details such as secure payment, SSL protection, and return policy.
    
    - Refine the layout to present pricing and action elements more clearly and professionally.

--- Add_To_Cart_page/src/components/Cart.jsx
+++ Add_To_Cart_page/src/components/Cart.jsx
@@ -85,12 +85,9 @@ function Cart() {
                     </form>
                 </div>
 
-
-
-                
             </div>
 
-            <div className='w-full sm:w-1/2'>
+            <div className='w-full sm:w-1/2 flex flex-col gap-4'>
                 
                 <div className='p-6 border border-neutral-300 rounded-md shadow-xs'>
                     <h1 className='text-xl font-bold mb-4'>Order Summary</h1>
@@ -128,6 +125,64 @@ function Cart() {
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div className='p-4 border border-neutral-300 rounded-md shadow-xs'>
+                    <h1 className='text-xl font-bold mb-4'>Order Total</h1>
+
+                    <div className='w-full flex flex-col gap-2'>
+                        <div className='w-full flex items-center justify-between gap-2'>
+                            <h3 className='text-md font-semibold'>Subtotal</h3>
+                            <span className='text-md text-neutral-500'>$2096.00</span>
+                        </div>
+
+                        <div className='w-full flex items-center justify-between gap-2'>
+                            <h3 className='text-md font-semibold'>Shipping</h3>
+                            <span className='text-md text-neutral-500'>$15.00</span>
+                        </div>
+
+                        <div className='w-full flex items-center justify-between gap-4'>
+                            <h3 className='text-md font-semibold'>Tax</h3>
+                            <span className='text-md text-neutral-500'>$167.68</span>
+                        </div>
+
+                        <div className='h-[1px] mt-2 w-full bg-neutral-300'></div>
+
+                        <div className='w-full flex items-center justify-between gap-4'>
+                            <h2 className='text-xl font-bold'>Total</h2>
+                            <span className='text-xl font-bold'>$2278.68</span>
+                        </div>
+                    </div>
+
+                    <div className='w-full mt-4 px-2 py-1 text-xl text-blue-700 flex items-center gap-2 bg-blue-300/20 backdrop:blur-lg rounded-lg'>
+                        <i class="ri-truck-line"></i>
+                        <p className='text-sm'>Free shipping on order over $500</p>
+                    </div>
+                </div>
+
+                <div className='w-full p-4 border border-neutral-300 rounded-md shadow-xs flex items-center gap-4'>
+                    <div className='text-green-500 flex items-center gap-2'>
+                        <i class="ri-shield-line"></i>
+                        <p className='text-sm text-neutral-500'>Secure Payment</p>
+                    </div>
+
+                    <div className='text-red-500 flex items-center gap-2'>
+                        <i class="ri-rotate-lock-fill"></i>
+                        <p className='text-sm text-neutral-500'>SSL Encripted</p>
+                    </div>
+
+                    <div className='text-blue-500 flex items-center gap-2'>
+                        <i class="ri-truck-line"></i>
+                        <p className='text-sm text-neutral-500'>8 days return polacy</p>
+                    </div>
+                </div>
+
+                <div className='w-full py-2 text-white bg-neutral-900 flex items-center justify-center rounded-md cursor-pointer shadow-xs'>
+                    <h2 className='text-md'>Place Order</h2>
+                </div>
+
+                <div className='w-full py-2 border border-neutral-300 flex items-center justify-center rounded-md cursor-pointer shadow-xs'>
+                    <h2 className='text-md'>Continue Shopping</h2>
                 </div>
 
             </div>
